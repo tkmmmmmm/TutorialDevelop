14e4626f882627c14fec55bf49434eee11d89892
tkmmmmmm <takumu.matsuoka.d308@dirbato.co.jp>
2024-04-24 14:37:04 +0900

    updateを登録

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -21,11 +21,16 @@ public class UserService {
     }
 
     // ----- 追加:ここから -----
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    // ----- 追加:ここまで -----
+
+
     /** Userの登録を行う */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
-    // ----- 追加:ここまで -----
-
 }
