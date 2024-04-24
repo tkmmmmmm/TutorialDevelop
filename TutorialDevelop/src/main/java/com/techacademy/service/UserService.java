740efadb3153cb2d40688d494ad9986dbd57775f
tkmmmmmm <takumu.matsuoka.d308@dirbato.co.jp>
2024-04-24 14:15:58 +0900

    registerを登録

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -18,4 +19,13 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
+
 }
